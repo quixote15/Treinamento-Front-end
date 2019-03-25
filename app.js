4f81c07f3dec02aa57020e8ed66a15134e89781d
quixote15 <santos2tiago@gmail.com>
2019-03-24 23:27:39 -0300

    Renderiza todos com ng-repeat

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ angular.module('trelloApp', [])
 
 .controller('TrelloController',['$scope', function($scope) {
     $scope.title = 'Trello';
+
+    $scope.todos = ["Aprender Framework", "Fazer café", "Iniciar projeto"];
+
+
   }])
